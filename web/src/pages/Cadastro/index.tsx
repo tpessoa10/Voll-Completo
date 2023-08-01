cc60e57bd8119f711ede8113e89368af9a81c677
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-08-01 11:58:51 -0300

    fazendo formularios

--- web/src/pages/Cadastro/index.tsx
+++ web/src/pages/Cadastro/index.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import Logo from '../../components/Cabecalho/assets/logo.png'
 import { Step, StepLabel, Stepper } from '@mui/material'
 import { useState } from 'react';
+import CampoDigitacao from '../../components/CampoDigitacao';
 
 
 const Imagem = styled.img`
@@ -18,8 +19,31 @@ const StepCustomizada = styled.div<PropsCustomizadas>`
     border-radius: 50%;
 `
 
+const Titulo = styled.h2`
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    color: var(--cinza);
+`
+
+const Formulario = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%
+`
+
 export default function Cadastro(){
     const [etapaAtiva, setEtapaAtiva] = useState(0)
+    const [nome, setNome] = useState('')
+    const [cnpj, setCnpj] = useState('')
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setEtapaAtiva(etapaAtiva + 1)
+    }
+
     return (
         <>
             <Imagem src={Logo} alt='Logo da Voll'/>
@@ -34,7 +58,12 @@ export default function Cadastro(){
                         <StepCustomizada cor={props.active ? 'lightblue' : 'lightgray'} />
                     )}/>
                 </Step>
-            </Stepper>
+            </Stepper> 
+            <Titulo>Primeiro, alguns dados básicos</Titulo>
+            <Formulario>
+                <CampoDigitacao tipo='text' valor={nome} placeholder='Insira o nome da clínica' onChange={setNome} label='Nome'/>
+                <CampoDigitacao tipo='text' valor={cnpj} placeholder='Insira o CNPJ' onChange={setCnpj} label='CNPJ'/>
+            </Formulario>
         </>
     )
 }
