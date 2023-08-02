9c7a6e1d567ec00438390fe933c5f8e9c569dbf9
Thiago <tpessoa96@gmail.com>
2023-08-01 21:09:45 -0300

    fazendo cadastro

--- web/src/pages/Cadastro/index.tsx
+++ web/src/pages/Cadastro/index.tsx
@@ -4,6 +4,18 @@ import { Step, StepLabel, Stepper } from '@mui/material'
 import { useState } from 'react';
 import CampoDigitacao from '../../components/CampoDigitacao';
 
+const BotaoEstilizado = styled.button`
+    width: 40%;
+    background-color: var(--azul-escuro);
+    border-radius: 8px;
+    padding: 12px 16px;
+    color: var(--branco);
+    border: none;
+    margin-top: 1em;
+    font-weight: 700;
+    line-height: 19px;
+`
+
 
 const Imagem = styled.img`
     padding: 1.5em 0 2em 0;
@@ -26,17 +38,31 @@ const Titulo = styled.h2`
     color: var(--cinza);
 `
 
-const Formulario = styled.div`
+const Formulario = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 80%
+`
+const Container = styled.div`
+    width: 100%;
+`
+
+const CampoDigitacaoPersonalizado = styled(CampoDigitacao)`
+    width: 20%;
 `
 
 export default function Cadastro(){
     const [etapaAtiva, setEtapaAtiva] = useState(0)
     const [nome, setNome] = useState('')
     const [cnpj, setCnpj] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [senhaVerificada, setSenhaVerificada] = useState('')
+    const [telefone, setTelefone] = useState('')
+    const [cep, setCep] = useState('')
+    const [rua, setRua] = useState('')
+    const [numero, setNumero] = useState('')
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -63,6 +89,19 @@ export default function Cadastro(){
             <Formulario>
                 <CampoDigitacao tipo='text' valor={nome} placeholder='Insira o nome da clínica' onChange={setNome} label='Nome'/>
                 <CampoDigitacao tipo='text' valor={cnpj} placeholder='Insira o CNPJ' onChange={setCnpj} label='CNPJ'/>
+                <CampoDigitacao tipo='text' valor={email} placeholder='Insira o Email' onChange={setEmail} label='Email'/>
+                <CampoDigitacao tipo='password' valor={senha} placeholder='Digite sua senha' onChange={setSenha} label='Crie uma senha'/>
+                <CampoDigitacao tipo='password' valor={senhaVerificada} placeholder='Digite sua senha novamente' onChange={setSenhaVerificada} label='Repita a senha'/>
+                <BotaoEstilizado>Criar Conta</BotaoEstilizado>
+            </Formulario>
+            <Titulo>Agora, os dados técnicos</Titulo>
+            <Formulario>
+                <CampoDigitacao tipo='tel' valor={telefone} placeholder='(DDD)XXXXX-XXXX' onChange={setTelefone} label='Telefone'/>
+                <CampoDigitacao tipo='number' valor={cep} placeholder='Informe o CEP' onChange={setCep} label='CEP'/>
+                <CampoDigitacao tipo='text' valor={rua} placeholder='Rua' onChange={setRua} label='Endereço'/>
+                <Container>
+                        <CampoDigitacao tipo='number' valor={numero} placeholder='Número' onChange={setNumero}/>
+                </Container>
             </Formulario>
         </>
     )
